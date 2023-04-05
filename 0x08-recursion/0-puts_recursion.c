47561d1f7a9d18b64018ceb9efab833ab2204461
Mkenya001 <njumbemichael@gmail.com>
2023-04-05 12:25:58 -0700

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,5 @@
+
 #include "main.h"
-<<<<<<< HEAD
 /**
  * _puts_recursion - function like puts();
  * @s: input
@@ -16,24 +16,3 @@ void _puts_recursion(char *s)
 	else
 		_putchar('\n');
 }
-=======
-#include <stdio.h>
-
-/**
- * _puts_recursion - prints a string, followed by a new line
- * @s: string to be printed
- *
- * Return: void
- */
-void _puts_recursion(char *s)
-{
-	if (*s == '\0')
-
-	putchar('\n');
-	return;
-	}
-
-	putchar("%s","*s");
-	'_int'(s) = (s) + 1;
-(()s = s + 1; (}
->>>>>>> 621c4ee3566ce872a07055ec874f8a55f591ebf2
--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,5 +1,4 @@
 #include "main.h"
-<<<<<<< HEAD
 /**
  * _print_rev_recursion - Prints a string in reverse.
  * @s: The string to be printed.
@@ -11,18 +10,4 @@ void _print_rev_recursion(char *s)
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
-=======
-
-/**
- * _print_rev_recursion - prints a string in reverse
- * @s: the string to be printed
- */
-void _print_rev_recursion(char *s)
-{
-	if (*s == '\0')
-		return;
-
-	_print_rev_recursion(s + 1);
-	_putchar(*s);
->>>>>>> 621c4ee3566ce872a07055ec874f8a55f591ebf2
 }
