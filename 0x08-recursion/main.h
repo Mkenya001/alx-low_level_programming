74685234b91e24e5ed8eafbfc153be57758ab510
Mkenya001 <njumbemichael@gmail.com>
2023-04-05 12:12:41 -0700

    recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,21 +1,13 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * _puts_recursion - prints a string, followed by a new line
- * @s: pointer to a string
- */
-
-void _puts_recursion(char *s)
-{
-        char *s1 = s;
-
-        if (*s1 != '\0')
-        {
-                _putchar(*s1);
-                s1++;
-                _puts_recursion(s1);
-        }
-        else
-                _putchar('\n');
-}
-
+void _puts_recursion(char *s);
+int _putchar(char c);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+#endif
